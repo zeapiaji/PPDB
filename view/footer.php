3719b90cf2cd49aeace538f7e3f0b640e2389bb9
armieeh <armisaniasinarya@gmail.com>
2021-12-27 09:46:38 +0700

    update armi

---
+++ view/footer.php
@@ -0,0 +1,63 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h3>SMKS MAHAPUTRA CERDAS UTAMA</h3>
+        <p>
+          Jl.Katapang-Andir Km.4 Kp.Pasantren Desa Sukamukti Kec.Katapang, Kab.Bandung<br><br>
+        </p>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links mx-5">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="#hero">Beranda</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#jurusan">Jurusan</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#about">Profile</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#galeri">Galeri</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#news">Berita</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#contact">Contact</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#pricing">PPDB</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-4 col-md-6 footer-newsletter">
+        <h4>Senin s.d Jumat</h4>
+        <p>Pukul 08.00 - 16.00 WIB</p>
+        <h4>Sabtu</h4>
+        <p>Pukul 09.00 - 15.00 WIB</p>
+        <h4>Minggu / Tanggal Merah</h4>
+        <p>LIBUR</p>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="container d-md-flex py-4">
+
+  <div class="me-md-auto text-center text-md-start">
+    <div class="copyright">
+      &copy; Copyright <strong><span>OnePage</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/ -->
+      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+    </div>
+  </div>
+  <div class="social-links text-center text-md-right pt-3 pt-md-0">
+    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+  </div>
+</div>
+</footer><!-- End Footer -->
---
+++ view/navbar.php
@@ -0,0 +1,24 @@
+<nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li class="dropdown"><a href="#jurusan"><span>Jurusan</span><i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#multimedia">Multimedia</a></li>
+              <li><a href="#rpl">Rekayasa Perangkat Lunak</a></li>
+            </ul>
+          <li class="dropdown"><a href="#about"><span>Profile</span><i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#about">Sekolah</a></li>
+              <li><a href="#guru">Orang Penting</a></li>
+            </ul>
+          <li><a class="nav-link scrollto" href="#services">Ekstrakulikuler</a></li>
+          <li><a class="nav-link scrollto" href="#galeri">Galeri</a></li>
+          <li><a class="nav-link scrollto" href="#news">Berita</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+          <li><a class="getstarted scrollto" href="#pricing">PPDB</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+</header><!-- End Header -->
